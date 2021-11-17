a87ad41c80ca59db82bb33994dc4e6931b85f140
Saranya0610 <93171922+Saranya0610@users.noreply.github.com>
2021-11-17 00:40:03 -0800

    Add files via upload

---
+++ Assignment04/index.js
@@ -0,0 +1,13 @@
+console.log("// Creation of Object calc");
+
+
+
+const product1=require('./calculator');
+
+
+
+const p2=new product1((10,20),(20,10),(10,20),(20,10));
+
+
+
+console.log(p2.display());
